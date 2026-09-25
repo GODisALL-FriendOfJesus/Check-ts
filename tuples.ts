16263778951c27a0990f03d6d6f68b00628bae16
Iradukunda Malaikaj <malaikajiradukunda@gmail.com>
2026-09-25 16:32:42 +0300

    GODISALL 'tuple section is well started'

---
+++ tuples.ts
@@ -0,0 +1,9 @@
+//tuples are arrays with fixed length and they are supported by type script
+//eg
+const tuples:[number,string]=[1,'hello']
+console.log(tuples);
+//👀🐱‍🚀
+//if the lenght does not mutch this will read type error🌾
+//const tupled:[number,number]=[1];
+// there will be an error explaining that there is requiremnts of numbers yet visile one 
+//is one
